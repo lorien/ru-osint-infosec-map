b01f169b19f54539424dfe5673da8e4305e739ba
Some User <guest@example.com>
2021-12-04 23:08:43 +0300

    Initial

---
+++ data/nodes.js
@@ -0,0 +1,118 @@
+dataNodes = [
+    {
+        "id": "scope-ru-osint",
+        "type": "scope",
+        "name": "RU OSINT",
+        "links": [
+            "user-madspectator",
+            "user-soxoj",
+            "user-agent-noreply",
+        ],
+        "imageUrl": "static/logo/scope-house.png",
+        "url": null,
+    },
+    {
+        "id": "user-madspectator",
+        "type": "user",
+        "name": "madspectator",
+        "links": [
+            "channel-grablab-osint-news",
+            "channel-grablab-osint-news-ru",
+            "group-grablab-osint",
+            "group-grablab-osint-ru"
+        ],
+        "imageUrl": "static/logo/user.jpg",
+        "url": "https://t.me/madspectator"
+    },
+    {
+        "id": "channel-grablab-osint-news",
+        "name": "GrabLab OSINT News",
+        "type": "channel",
+        "links": [
+            "group-grablab-osint"
+        ],
+        "imageUrl": "static/logo/group-grablab-osint-news.jpg",
+        "url": "https://t.me/grablab_osint_news"
+    },
+    {
+        "id": "channel-grablab-osint-news-ru",
+        "name": "GrabLab OSINT Ru Новости",
+        "type": "channel",
+        "links": [
+            "group-grablab-osint-ru"
+        ],
+        "imageUrl": "static/logo/group-grablab-osint-news-ru.jpg",
+        "url": "https://t.me/grablab_osint_news_ru"
+    },
+    {
+        "id": "group-grablab-osint",
+        "name": "GrabLab OSINT Chat",
+        "type": "group",
+        "links": [],
+        "imageUrl": "static/logo/group-grablab-osint.jpg",
+        "url": "https://t.me/grablab_osint"
+    },
+    {
+        "id": "group-grablab-osint-ru",
+        "name": "GrabLab OSINT Ru Чат",
+        "type": "group",
+        "links": [],
+        "imageUrl": "static/logo/group-grablab-osint-ru.jpg",
+        "url": "https://t.me/grablab_osint_ru"
+    },
+    {
+        "id": "user-agent-noreply",
+        "name": "Agent",
+        "type": "user",
+        "links": [
+            "group-howtofind-chat",
+            "channel-howtofind",
+            "channel-howtofind-ru",
+        ],
+        "imageUrl": "static/logo/user-agent-noreply.jpg",
+        "url": "https://t.me/agent_noreply"
+    },
+    {
+        "id": "group-howtofind-chat",
+        "name": "Public HowToFind Group",
+        "type": "group",
+        "links": [],
+        "imageUrl": "static/logo/group-howtofind-chat.jpg",
+        "url": "https://t.me/howtofind_chat"
+    },
+    {
+        "id": "channel-howtofind",
+        "name": "HowToFind Channel",
+        "type": "channel",
+        "links": [],
+        "imageUrl": "static/logo/channel-howtofind.jpg",
+        "url": "https://t.me/howtofind"
+    },
+    {
+        "id": "channel-howtofind-ru",
+        "name": "HowToFind на русском",
+        "type": "channel",
+        "links": [],
+        "imageUrl": "static/logo/channel-howtofind-ru.jpg",
+        "url": "https://t.me/howtofind_ru"
+    },
+    {
+        "id": "user-soxoj",
+        "name": "Soxoj",
+        "type": "user",
+        "links": [
+            "channel-osint-mindset",
+        ],
+        "imageUrl": "static/logo/user-soxoj.jpg",
+        "url": "https://t.me/soxoj"
+    },
+    {
+        "id": "channel-osint-mindset",
+        "name": "OSINT mindset",
+        "type": "channel",
+        "links": [
+        ],
+        "imageUrl": "static/logo/channel-osint-mindset.jpg",
+        "url": "https://t.me/osint_mindset"
+    },
+];
